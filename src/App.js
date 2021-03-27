4f43036efcd88f0731269dec2fb4005e35e942ec
Aaron Diggdon <aarondiggdon@gmail.com>
2021-03-26 19:13:24 -0700

    Added landing component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
-import './App.css';
-import { useRef, useLayoutEffect, useEffect, useState } from 'react';
+import "./App.css";
+import Wrapper from "./components/Wrapper";
+import Landing from "./components/Landing";
+import { useRef, useLayoutEffect, useEffect, useState } from "react";
 
 function App() {
-const scroller = useRef();
-
-const [touch, setTouch] = useState(false)
-
+  const scroller = useRef();
+  const [page, setPage] = useState();
 
   let position = 0;
   let rounded = 0;
   let speed = 0;
 
+  const data = {
+    current: 0,
+    previous: 0,
+    rounded: 0,
+    ease: .2
+    
+  }
   
-  window.addEventListener('wheel', (e)=>{
-    speed = e.deltaY * .0005
-   })
 
-
-
-  
+  window.addEventListener("wheel", (e) => {
+    speed = e.deltaY * 0.0005;
+  });
 
   const scrolling = () => {
+    position += speed;
+
+    //INERTIA: speed less than 1
+    speed *= 0.8;
+    rounded = Math.round(position);
+    console.log(position)
+    if (position < 0) {
+      position = 0;
+    }
+    if (position > 4) {
+      rounded = 4;
+    }
+    let difference = rounded - position;
+
+   
+    //SLIDE BACK SPEED
+    position += difference * 0.05;
     
-  position += speed;
+    // PAGE TRANSITION: -Math.pow((position - PAGENUM), 2) + PAGENUM
 
-  //INERTIA: speed less than 1
-  speed *=.8;
-  
-  rounded = Math.round(position);
-  
-  let difference = (rounded - position);
- 
-  //SLIDE BACK SPEED
-  position += difference*.05;
-  scroller.current.style.transform = `translate3d(0, ${position*100}px, 0)`
-  
-  console.log(difference)
+    scroller.current.style.transform = `translateY(-${position*100}vh)`;
+    setPage(rounded);
+    requestAnimationFrame(() => scrolling());
+  };
 
-  requestAnimationFrame(()=> scrolling());
-  }
-  useEffect(()=>{
+  useEffect(() => {
     scrolling();
-    },[])
+  }, []);
 
   return (
     <div className="App">
-      <div className="scroller" ref={scroller}></div>
+      <div id="scroller" ref={scroller}>
+      <Landing />
+      <div className="page"></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Landing/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Landing(props) {
+    return <div id="landing" >
+      <header className="helloC">
+        <h1 className="hello">hello!</h1>
+      </header>
+    </div>;
+  }
+  
+  export default Landing;
